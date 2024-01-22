221f30b73091169b32087ae714c759f1d0894a1b
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-01-22 10:39:02 +0700

    [UPDATE] add encapsulation

--- src/com/enigma/oop_java/Car.java
+++ src/com/enigma/oop_java/Car.java
@@ -2,26 +2,56 @@ package com.enigma.oop_java;
 
 public class Car {
     /*
-    * Tahapan pertama membuat object adalah membuat blueprint
-    * */
+     * Tahapan pertama membuat object adalah membuat blueprint
+     * */
 
     // Properti atau Attribute
-    public String brand;
-    public String color;
-    public Integer tier;
+    private String brand;
+    private String color;
+    private Integer tier;
+    private Integer fuel;
 
-    public Car (String brand, String color, Integer tier) { // --> Constructor
+    public Car(String brand, String color, Integer tier) { // --> Constructor
         this.brand = brand; // this memanggil dirinya sendiri dari class
         this.color = color; // karena nama parameternya sama, kecuali kalo beda
         this.tier = tier; // constructor itu harus berurutan ketika dipanggil sama objectnya
+        this.fuel = 0;
+    }
+
+    // Contoh Getter biasanya cuma sebaris, tapi bisa dikasih validasi
+    public Integer getFuel(){
+        if (fuel == null){
+            return 0;
+        } else {
+            return fuel;
+        }
+    }
+
+    // Contoh Setter. Sama juga, biasanya cuma sebaris, tapi bisa dikasih validasi
+    public void setFuel(Integer fuel){
+        if (fuel >  30 || fuel <= 0){
+            System.out.println("Tidak boleh minus dan tangki kepenuhan!");
+        } else {
+            System.out.println("Mobil Bisa jalan!");
+        }
+        this.fuel += fuel;
     }
 
     // Method atau Fungsi
-    public void start(){
-        System.out.println("Brum");
+    public void start() {
+
+        if (fuel >  30 || fuel <= 0){
+            System.out.println("Tangki bermasalah");
+        } else {
+            System.out.println("Jalan!");
+        }
     }
 
-    public void stop(){
+    public void stop() {
         System.out.println("Cittt");
+    }
+
+    public String print(){
+        return "Brand : " + brand ;
     }
 }
--- src/com/enigma/oop_java/Main.java
+++ src/com/enigma/oop_java/Main.java
@@ -2,19 +2,16 @@ package com.enigma.oop_java;
 
 public class Main {
     public static void main(String[] args) {
-        // Membuat object sama seperti membuat variable
-        // Membuat object disebut juga dengan instance
-        // Class itu tipe data non primitive
-        // Keyword untuk membuat object adalah "new"
 
-        Car honda = new Car("Jazz","Black",4); // -> instance class
+        Car ayla = new Car("Daihatsu","Silver",4);
 
-        // Cara 1 - reassign
-        // honda.brand = "Jazz";
-        // honda.color = "Black";
-        // honda.tier = 4;
+        // Before Encapsulation
+        // ayla.fuel = 50; // direct access
 
-        // Cara 2 - constructor
+
+        ayla.setFuel(25);
+        ayla.start();
+        System.out.println(ayla.getFuel());
 
     }
 }
